e4fc4464bd42b059eec310fb0b99552debb57c30
Megan Allas <allasmct@gmail.com>
2024-03-26 11:00:33 -0400

    generic button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Button from "@/components/Button";
+import Link from "next/link";
 
 export default function Home() {
   return (
       <div className="h-screen flex flex-col items-center justify-center">
         <h1 className="text-6xl font-extrabold text-blue-500 mb-1">Web Chat</h1>
         <p className="mb-10">subheading</p>
-        <Button>Start chat</Button>
+        <Button as={Link} href="/chat">Start chat</Button>
       </div>
   );
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,28 @@
 import { ComponentPropsWithoutRef } from "react";
 import { twMerge } from "tailwind-merge";
 
-export default function Button(props: ComponentPropsWithoutRef<"button">) {
+// Generic type but only allow components.
+interface ButtonProps<T extends React.ElementType> {
+  as?: T;
+}
+
+export default function Button<T extends React.ElementType = "button">({
+  as,
+  ...props
+}: ButtonProps<T> & Omit<ComponentPropsWithoutRef<T>, keyof ButtonProps<T>>) {
+  // If any props within ButtonProps are also in the component props, it will prioritize ButtonProps.
+  // Essentially removing any duplicate prop keys.
+
+  const Component = as || "button";
+
   return (
     // Tailwind Merge package to allow prop classes to override default styling.
-    <button
+    <Component
       {...props}
       className={twMerge(
         "flex items-center justify-center gap-2 rounded bg-blue-500 text-white p-[0.875rem] active:bg-blue-600 disabled:bg-gray-200",
         props.className
       )}
-    ></button>
+    ></Component>
   );
 }
